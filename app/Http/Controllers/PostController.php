a7faad932279845a02a7999847cf067cbcb1e7cf
vinopermana77 <vinopermana13@gmail>
2022-11-27 13:29:36 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostDetailResource;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        // return response()->json(['data' => $posts]);
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username'));
+    }
+
+    public function show($id)
+    {
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        return new PostDetailResource($post);
+    }
+
+    // public function show2($id)
+    // {
+    //     $post = Post::findOrFail($id);
+    //     return new PostDetailResource($post);
+    // }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required',
+        ]);
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required',
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+}
